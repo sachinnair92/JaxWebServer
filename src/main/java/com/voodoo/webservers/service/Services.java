9e350e5d2e640e5b0c9975e484a78f26d57f5646
sachinnair92 <sachinnair92@gmail.com>
2016-05-26 05:33:50 +0530

    added patient and band api's

--- src/main/java/com/voodoo/webservers/service/Services.java
+++ src/main/java/com/voodoo/webservers/service/Services.java
@@ -2,6 +2,7 @@ package com.voodoo.webservers.service;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.ws.rs.QueryParam;
 
 /**
  * @author chamerling
@@ -15,5 +16,23 @@ public interface Services {
 
 	@WebMethod
 	String register_user( String User_Name,String Password, String Hospital_name,String Type_of_User);
+
+	@WebMethod
+	String register_ambulance(String User_Name,String Password, String Hospital_name,String Type_of_User,String ambulance_id) ;
+
+	@WebMethod
+	String add_new_patient( String hospital_name, String ambulance_id , String p_name, String gender, String blood_grp,String condition,String problem,String police_case,String is_enabled);
+
+	@WebMethod
+	String update_patient( String hospital_name, String ambulance_id , String p_name, String p_id, String gender, String blood_grp,String condition,String problem,String police_case,String is_enabled);
+
+	@WebMethod
+	String get_patient_details( String hospital_name, String ambulance_id , String p_id) ;
+
+	@WebMethod
+	String update_heartrate(String hospital_name, String ambulance_id ,String p_id, String heartrate) ;
+
+	@WebMethod
+	String get_heartrate(String hospital_name, String ambulance_id , String p_id) ;
 
 	}
--- src/main/java/com/voodoo/webservers/service/ServicesImpl.java
+++ src/main/java/com/voodoo/webservers/service/ServicesImpl.java
@@ -15,12 +15,14 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Random;
 
 /**
  * @author chamerling
@@ -31,10 +33,10 @@ public class ServicesImpl implements Services {
     MongoClientURI connectionString = new MongoClientURI("mongodb://voodoo:722446@ds057862.mlab.com:57862/prms");
     MongoClient mongoClient = new MongoClient(connectionString);
     MongoDatabase db = mongoClient.getDatabase(connectionString.getDatabase());
-
-    int is_valid=0;
     String pwd;
-
+    int is_valid=0;
+    boolean patientfound=false;
+    boolean datafound;
 
 
 	@Override
@@ -129,20 +131,330 @@ public class ServicesImpl implements Services {
         return "null";
     }
 
+    @Override
+    public String register_ambulance(String User_Name, String Password, String Hospital_name, String Type_of_User, String ambulance_id) {
+        MongoCollection<org.bson.Document> collection = db.getCollection("credentials");
+        try {
+
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            Document doc = icBuilder.newDocument();
+            Element node = doc.createElement("message");
+
+            org.bson.Document doc1 = new org.bson.Document("user_name", User_Name)
+                    .append("password", Password)
+                    .append("hospital_name", Hospital_name)
+                    .append("type_of_user", Type_of_User)
+                    .append("ambulance_id", ambulance_id);
+            collection.insertOne(doc1);
+            node.appendChild(doc.createTextNode("true"));
+            doc.appendChild(node);
+            return convertDocumentToString(doc);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return "null";
+    }
+
+    public int gen() {
+        Random r = new Random( System.currentTimeMillis() );
+        return 10000 + r.nextInt(20000);
+    }
+
+
+    @Override
+    public String add_new_patient(String hospital_name, String ambulance_id, String p_name, String gender, String blood_grp, String condition, String problem, String police_case, String is_enabled) {
+
+        MongoCollection<org.bson.Document> collection = db.getCollection("patient_details");
+        try {
+
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            Document doc = icBuilder.newDocument();
+
+            String p_id="Patient_"+String.valueOf(gen());
+            if(p_name==null)
+            {
+                p_name=p_id;
+            }
+
+            org.bson.Document doc1 = new org.bson.Document("hospital_name", hospital_name)
+                    .append("ambulance_id", ambulance_id)
+                    .append("p_name", p_name)
+                    .append("p_id", p_id)
+                    .append("gender", gender)
+                    .append("blood_grp", blood_grp)
+                    .append("condition", condition)
+                    .append("problem", problem)
+                    .append("police_case", police_case)
+                    .append("is_enabled", is_enabled);
+
+            collection.insertOne(doc1);
+            Element node = doc.createElement("P_id");
+            node.appendChild(doc.createTextNode(p_id));
+            doc.appendChild(node);
+            node = doc.createElement("message");
+            node.appendChild(doc.createTextNode("true"));
+            doc.appendChild(node);
+            return convertDocumentToString(doc);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return "null";
+    }
+
+    @Override
+    public String update_patient(String hospital_name, String ambulance_id, String p_name, String p_id, String gender, String blood_grp, String condition, String problem, String police_case, String is_enabled) {
+
+        MongoCollection<org.bson.Document> collection = db.getCollection("patient_details");
+        try {
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            Document doc = icBuilder.newDocument();
+
+            UpdateResult ur = collection.updateOne(new org.bson.Document("p_id", p_id).append("hospital_name", hospital_name).append("ambulance_id",ambulance_id), new org.bson.Document("$set", new org.bson.Document("hospital_name", hospital_name).append("ambulance_id", ambulance_id).append("p_name",p_name).append("gender",gender).append("blood_grp",blood_grp).append("condition",condition).append("problem",problem).append("police_case",police_case).append("is_enabled",is_enabled)));
+
+
+            if (ur.getModifiedCount() != 0) {
+                Element node = doc.createElement("message");
+                node.appendChild(doc.createTextNode("true"));
+                doc.appendChild(node);
+                return convertDocumentToString(doc);
+            }
+
+            Element node = doc.createElement("message");
+            node.appendChild(doc.createTextNode("false"));
+            doc.appendChild(node);
+            return convertDocumentToString(doc);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return "null";
+    }
+
+    Document doc1;
+
+    @Override
+    public String get_patient_details(String hospital_name, String ambulance_id, String p_id) {
+
+        MongoCollection<org.bson.Document> collection = db.getCollection("patient_details");
+        try {
+
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            doc1 = icBuilder.newDocument();
+
+
+            FindIterable<org.bson.Document> iterable = collection.find(new org.bson.Document("hospital_name", hospital_name).append("ambulance_id",ambulance_id).append("p_id",p_id));
+
+
+            patientfound = false;
+            iterable.forEach(new Block<org.bson.Document>() {
+                @Override
+                public void apply(final org.bson.Document document) {
+                    patientfound = true;
+
+                    Element node = doc1.createElement("hospital_name");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("hospital_name"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("ambulance_id");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("ambulance_id"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("p_name");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("p_name"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("p_id");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("p_id"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("gender");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("gender"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("blood_grp");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("blood_grp"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("condition");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("condition"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("problem");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("problem"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("police_case");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("police_case"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("is_enabled");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("is_enabled"))));
+                    doc1.appendChild(node);
+
+                }
+
+            });
+
+
+            if (patientfound) {
+                Element node = doc1.createElement("message");
+                node.appendChild(doc1.createTextNode("true"));
+                doc1.appendChild(node);
+                return convertDocumentToString(doc1);
+            }
+
+            Element node = doc1.createElement("message");
+            node.appendChild(doc1.createTextNode("false"));
+            doc1.appendChild(node);
+            return convertDocumentToString(doc1);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
 
 
+        return "null";
+    }
+
+    @Override
+    public String update_heartrate(String hospital_name, String ambulance_id, String p_id, String heartrate) {
+
+        MongoCollection<org.bson.Document> collection = db.getCollection("heartrate_details");
+        try {
+
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            Document doc = icBuilder.newDocument();
 
 
+            FindIterable<org.bson.Document> iterable = collection.find(new org.bson.Document("hospital_name", hospital_name).append("ambulance_id",ambulance_id).append("p_name",p_id));
+
+            datafound=false;
+            iterable.forEach(new Block<org.bson.Document>() {
+                @Override
+                public void apply(final org.bson.Document document) {
+                    datafound=true;
+                }
+            });
 
 
+            if(datafound==true)
+            {
+                UpdateResult ur = collection.updateOne(new org.bson.Document("p_id", p_id).append("hospital_name", hospital_name).append("ambulance_id",ambulance_id), new org.bson.Document("$set", new org.bson.Document("heartrate", heartrate)));
+                if (ur.getModifiedCount() != 0) {
+                    Element node = doc.createElement("message");
+                    node.appendChild(doc.createTextNode("true"));
+                    doc.appendChild(node);
+                    return convertDocumentToString(doc);
+                }
+                Element node = doc.createElement("message");
+                node.appendChild(doc.createTextNode("false"));
+                doc.appendChild(node);
+                return convertDocumentToString(doc);
+            }else
+            {
+                org.bson.Document doc1 = new org.bson.Document("p_id", p_id)
+                        .append("hospital_name", hospital_name)
+                        .append("ambulance_id", ambulance_id)
+                        .append("heartrate",heartrate);
+                collection.insertOne(doc1);
+                Element node = doc.createElement("message");
+                node.appendChild(doc.createTextNode("true"));
+                doc.appendChild(node);
+                return convertDocumentToString(doc);
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return "null";
+    }
+
+    @Override
+    public String get_heartrate(String hospital_name, String ambulance_id, String p_id) {
+
+        MongoCollection<org.bson.Document> collection = db.getCollection("heartrate_details");
+        try {
+            DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder icBuilder;
+            icBuilder = icFactory.newDocumentBuilder();
+            doc1 = icBuilder.newDocument();
 
 
+            FindIterable<org.bson.Document> iterable = collection.find(new org.bson.Document("hospital_name", hospital_name).append("ambulance_id",ambulance_id).append("p_id",p_id));
+
+            datafound=false;
+            iterable.forEach(new Block<org.bson.Document>() {
+                @Override
+                public void apply(final org.bson.Document document) {
+
+                    datafound=true;
 
 
+                    Element node = doc1.createElement("hospital_name");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("hospital_name"))));
+                    doc1.appendChild(node);
+
+                    node = doc1.createElement("ambulance_id");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("ambulance_id"))));
+                    doc1.appendChild(node);
 
 
+                    node = doc1.createElement("p_id");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("p_id"))));
+                    doc1.appendChild(node);
 
+
+                    node = doc1.createElement("heartrate");
+                    node.appendChild(doc1.createTextNode(String.valueOf(document.get("heartrate"))));
+                    doc1.appendChild(node);
+
+                }
+            });
+
+
+            if(datafound==true)
+            {
+                Element node = doc1.createElement("message");
+                node.appendChild(doc1.createTextNode("true"));
+                doc1.appendChild(node);
+            }
+
+            Element node = doc1.createElement("message");
+            node.appendChild(doc1.createTextNode("false"));
+            doc1.appendChild(node);
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return "null";
+    }
 
 
     private static String convertDocumentToString(Document doc) {
